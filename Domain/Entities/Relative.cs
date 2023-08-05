3f7c582e3d8aa2c2419e238816eefb74d3a5784f
PedroPampolini <pedromendicino25@gmail.com>
2023-08-04 23:18:26 -0300

    Candidate and Relative Model added

---
+++ Domain/Entities/Relative.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AlfaCoreDumped.Domain.Entities
+{
+    public class Relative
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid CandidateId { get; set; }
+
+        [Required]
+        public string Cpf { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Kinship { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+    }
+}
