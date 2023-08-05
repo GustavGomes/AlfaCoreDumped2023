7139c254278867c89e068b6cbfe05318a6ec6b45
PedroPampolini <pedromendicino25@gmail.com>
2023-08-05 18:22:42 -0300

    Consertos e melhorias

--- service/EquipmentsService.py
+++ service/EquipmentsService.py
@@ -1,7 +1,10 @@
+# Responsável por receber as requisições de equipamentos e encaminhar para o DAO
+
 from flask import jsonify, Request, Response
 from dao import equipmentsDao
-from model.areasModel import Areas as Area
+from model.equipmentsModel import Equipment
 
+# Retorna um equipamento a partir do id
 def GetEquipmentById(request: Request) -> str:
     #id = request.json['id']
     id = request.args.get('id')
@@ -10,7 +13,8 @@ def GetEquipmentById(request: Request) -> str:
         return jsonify({'msg': 'Don\'t exist'})
     else:
         return jsonify(area.__dict__())
-    
+
+# Retorna um equipamento a partir do código dele
 def GetEquipmentByCode(request: Request) -> str:
     #code = request.json['code']
     code = request.args.get('code')
@@ -19,9 +23,10 @@ def GetEquipmentByCode(request: Request) -> str:
         return jsonify({'msg': 'Don\'t exist'})
     else:
         return jsonify(area.__dict__())
-    
+
+# Insere um equipamento vindo do front-end no banco de dados
 def InsertEquipment(request) -> Response:
-    a = Area(Id=request.json['id'], Code=request.json['code'],
+    a = Equipment(Id=request.json['id'], Code=request.json['code'],
              Description=request.json['description'], LiberationStatus=request.json['liberationStatus'],
              PdfFile=request.json['pdfFile'])
     return equipmentsDao.InsertEquipment(a)
--- service/UserService.py
+++ service/UserService.py
@@ -1,7 +1,12 @@
+# Responsável por receber as requisições do usuário e encaminhar para o DAO
+# 
+
 from flask import jsonify, Request, Response
 from dao import userDao
 from model.userModel import User
 
+# Recupera o username e password do usuário e verifica se existe no banco de dados
+# Se existir, retorna o usuário, se não, retorna uma mensagem de erro de credenciais inválidas
 def Login(request: Request) -> str:
     username = request.json['cpf']
     password = request.json['password']
@@ -10,7 +15,8 @@ def Login(request: Request) -> str:
         return jsonify({'msg': 'Invalid Credentials'})
     else:
         return jsonify(user.__dict__())
-    
+
+# Insere um usuário no banco de dados, apenas para testes
 def InsertUser(request) -> Response:
     u = User(Id=request.json['id'], Username=request.json['username'],
              Password=request.json['password'], Cpf=request.json['cpf'],
